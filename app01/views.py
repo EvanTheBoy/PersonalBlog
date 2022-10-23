c189a11f597c390f0e28dbcac2d6d0cae547c48c
Evan <761356554@qq.com>
2022-10-23 20:09:05 +0800

    实现了轮播图

--- app01/views.py
+++ app01/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def get_index(request):
-    return render(request, "index.html")
+    # 进行图片渲染
+    img_list = [
+        "/static/myFile/image/header/slideshow1.jpg",
+        "/static/myFile/image/header/slideshow2.jpg",
+        "/static/myFile/image/header/slideshow3.jpg",
+        "/static/myFile/image/header/slideshow4.jpg"
+    ]
+    return render(request, "index.html", {"img_list": img_list})

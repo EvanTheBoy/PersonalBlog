df243fed6957904504cccbb1c98d71dcf098de76
Evan <761356554@qq.com>
2022-10-25 19:30:05 +0800

    update

--- app01/templatetags/my_tag.py
+++ app01/templatetags/my_tag.py
@@ -3,6 +3,18 @@ from django import template
 register = template.Library()
 
 
+# 这里是自定义过滤器
 @register.filter
 def add1(item):
     return int(item) + 1
+
+
+@register.inclusion_tag('my_tag/headers.html')
+def banner(menu_name):
+    img_list = [
+        "/static/myFile/image/header/slideshow1.jpg",
+        "/static/myFile/image/header/slideshow2.jpg",
+        "/static/myFile/image/header/slideshow3.jpg",
+        "/static/myFile/image/header/slideshow4.jpg"
+    ]
+    return {"img_list": img_list}

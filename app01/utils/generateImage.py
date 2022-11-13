34617dcebd9e72ce2959e03731f0a85bf13f4fc9
Evan <761356554@qq.com>
2022-11-13 16:48:23 +0800

    为验证码添加点和线

--- app01/utils/generateImage.py
+++ app01/utils/generateImage.py
@@ -2,21 +2,50 @@ from PIL import Image, ImageDraw, ImageFont
 import string
 import random
 
+
+# 生成随机颜色，这里返回的是一个元组
+def generate_random_color():
+    return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+
+
 str_all = string.digits + string.ascii_letters
-# 生成一个白色的背景图片
-img = Image.new('RGB', (200, 50), color=(255, 255, 255))
 
-# 生成一个和白色背景图片大小一样的画布
-draw = ImageDraw.Draw(img)
 
-# 生成字体对象
-font = ImageFont.truetype(font='./font/New Asgard.ttf', size=32)
+# 生成随机验证码
+def generate_random_code():
+    # 生成一个白色的背景图片
+    width = 200
+    height = 50
+    img = Image.new('RGB', (width, height), color=(255, 255, 255))
 
-valid_code = ''
-for i in range(4):
-    random_char = random.choice(str_all)
-    valid_code += random_char
-    draw.text((40*i + 30, 0), random_char, (0, 0, 0), font=font)
+    # 生成一个和白色背景图片大小一样的画布
+    draw = ImageDraw.Draw(img)
 
-print(valid_code)
-img.save('new_img.png', 'PNG')
+    # 生成字体对象
+    font = ImageFont.truetype(font='./font/New Asgard.ttf', size=32)
+
+    # 书写文字
+    valid_code = ''
+    for i in range(4):
+        random_char = random.choice(str_all)
+        valid_code += random_char
+        draw.text((40 * i + 29, 0), random_char, (0, 0, 0), font=font)
+
+    print(valid_code)
+
+    # 随机生成点
+    for i in range(80):
+        x, y = random.randint(0, width), random.randint(0, height)
+        draw.point((x, y), generate_random_color())
+
+    # 随机生成线
+    for i in range(8):
+        x1, y1 = random.randint(0, width), random.randint(0, height)
+        x2, y2 = random.randint(0, width), random.randint(0, height)
+        draw.line((x1, y1, x2, y2), fill=generate_random_color())
+
+    img.save('new_img.png', 'PNG')
+
+
+if __name__ == '__main__':
+    generate_random_code()

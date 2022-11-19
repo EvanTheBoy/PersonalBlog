d9b40cc07f059b7526a476c5ddd83134ee1b5472
Evan <761356554@qq.com>
2022-11-19 16:31:38 +0800

    完成了登录代码的优化，开始编写操作数据库的注册代码

--- app01/views.py
+++ app01/views.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse, HttpResponse
 from app01.utils.generateCode import generate_random_code
 from django import forms
 from django.contrib import auth
+from app01.models import UserInfo
+
 
 # Create your views here.
 
@@ -28,9 +30,50 @@ class LoginForm(forms.Form):
         user = auth.authenticate(username=name, password=pwd)
         if not user:
             self.add_error("pwd", "用户名或密码错误!")
+        self.cleaned_data['user'] = user
         return self.cleaned_data
 
     # 验证码的验证应该属于另外的方法，因此不应该在全局钩子中编写
+    def clean_code(self):
+        code: str = self.cleaned_data.get('code')
+        valid_code: str = self.request.session.get('valid_code')
+        if code.upper() != valid_code.upper():
+            self.add_error("code", "验证码错误!")
+        return self.cleaned_data
+
+
+# 注册的字段验证
+class RegisterForm(forms.Form):
+    name = forms.CharField(error_messages={"required": "请输入用户名"})
+    pwd = forms.CharField(error_messages={"required": "请输入密码"})
+    re_pwd = forms.CharField(error_messages={"required": "请再次输入密码"})
+    code = forms.CharField(error_messages={"required": "请输入验证码"})
+
+    # 重写init方法,为了拿request,session里面保存的code.
+    def __init__(self, *args, **kwargs):
+        # 拿到请求对象, 并将其赋给这个类的request
+        self.request = kwargs.pop('request', None)
+
+        # 让这个方法正常执行
+        super().__init__(*args, **kwargs)
+
+    # 定义全局钩子，验证两次的密码是否一致
+    def clean(self):
+        pwd = self.cleaned_data.get('pwd')
+        re_pwd = self.cleaned_data.get('re_pwd')
+        if pwd != re_pwd:
+            self.add_error('re_pwd', '两次密码不一致!')
+        return self.cleaned_data
+
+    # 定义局部钩子，验证用户名是否已经被使用
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        user_query = UserInfo.objects.filter(username=name)
+        if user_query:
+            self.add_error("name", "该用户已注册!")
+        return self.cleaned_data
+
+    # 定义局部钩子，验证验证码是否正确
     def clean_code(self):
         code: str = self.cleaned_data.get('code')
         valid_code: str = self.request.session.get('valid_code')
@@ -71,6 +114,10 @@ def login(request):
             # 配置res的信息
             res['self'], res['msg'] = clean_form(form)
             return JsonResponse(res)
+        # 在这里实现登录功能
+        user = form.cleaned_data.get('user')
+        # 登录操作
+        auth.login(request, user)
         res['code'] = 0
         return JsonResponse(res)
     return render(request, "login.html")
@@ -87,4 +134,17 @@ def get_random_code(request):
 
 
 def register(request):
+    if request.method == 'POST':
+        res = {
+            'code': 1,  # 0表示登录成功
+            'msg': "登录成功!",  # 提示信息
+            'self': None  # 提示错误的地方
+        }
+        form = RegisterForm(request.data, request=request)
+        if not form.is_valid():
+            res['self'], res['msg'] = clean_form(form)
+            return JsonResponse(res)
+        # 注册成功
+        res['code'] = 0
+        return JsonResponse(res)
     return render(request, "register.html")

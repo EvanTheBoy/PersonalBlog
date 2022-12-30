ea630d713f52f22a222d7b33f43defccaa1a6a9f
Evan <761356554@qq.com>
2022-12-30 18:32:08 +0800

    update

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from api.views import login, article
 
 urlpatterns = [
-    path('login/', login.LoginView.as_view()),
-    path('register/', login.RegisterView.as_view()),
+    path('login/', login.LoginView.as_view()),  # 登录
+    path('register/', login.RegisterView.as_view()),  # 注册
     path('article/', article.ArticleView.as_view())  # 凡是到article的，都认为是与添加文章有关
 ]

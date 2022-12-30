badd1d2ecbc0590273f49c82f68be4fd71353a7e
Evan <761356554@qq.com>
2022-12-30 17:15:18 +0800

    开始编写后端页面

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from api.views import login
+from api.views import login, article
 
 urlpatterns = [
     path('login/', login.LoginView.as_view()),
     path('register/', login.RegisterView.as_view()),
+    path('article/', article.ArticleView.as_view())  # 凡是到article的，都认为是与添加文章有关
 ]
---
+++ api/views/article.py
@@ -0,0 +1,7 @@
+from django.views import View
+from django.http import JsonResponse
+
+
+class ArticleView(View):
+    def post(self, request):
+        return JsonResponse({})
